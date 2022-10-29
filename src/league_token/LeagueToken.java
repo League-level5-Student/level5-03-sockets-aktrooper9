a136c5062ecf0c0c15ed1c4d96d3ccf154e50f74
league <league@imac-14.attlocal.net>
2022-10-29 11:26:51 -0700

    s

--- src/league_token/LeagueToken.java
+++ src/league_token/LeagueToken.java
@@ -1,7 +1,7 @@
 package league_token;
 
 /*
- * Run this program to either save(encrypt) or get(decrypt) your token
+ * Run this program to either save(encrypt) or get(decrypt) your token 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
  */
 public class LeagueToken {
 	public static void main(String[] args) throws Exception {
